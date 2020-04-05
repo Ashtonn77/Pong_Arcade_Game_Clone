e2fa7d870236e55d0509bd9ba13a3a6e774fa7a2
ashton naidoo <ashtonrbay@gmail.com>
2020-04-05 20:05:10 +0200

    added index.js

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ let rightPaddle_y = 250;
 let leftPlayerScore = 0;
 let rightPlayerScore = 0;
 
+const WINNING_SCORE = 3;
+let winScreen = false;
+
 function calculateMouseMovePosition(evt){
     let rect = canvas.getBoundingClientRect();
     let root = document.documentElement;
@@ -24,6 +27,14 @@ function calculateMouseMovePosition(evt){
     return{
         x: mouse_x,
         y: mouse_y
+    }
+}
+
+function handleWin(){
+    if(winScreen){
+        leftPlayerScore = 0;
+        rightPlayerScore = 0;
+        winScreen = false;
     }
 }
 
@@ -45,10 +56,15 @@ window.onload = function(){
     }, frameRate);
 
     canvas.addEventListener('mousemove', leftPaddleMove);
+    canvas.addEventListener('mousedown', handleWin);
 
 }
 
 function ballReset(){
+    if(leftPlayerScore >= WINNING_SCORE ||
+        rightPlayerScore >= WINNING_SCORE){
+            winScreen = true;
+        }
     ballSpeed_x = -ballSpeed_x;
     ball_x = canvas.width / 2;
     ball_y = canvas.height / 2;
@@ -56,15 +72,18 @@ function ballReset(){
 
 function computerMove(){
     let rightPaddleCenter = rightPaddle_y + (PADDLE_HEIGHT / 2);
-    if(rightPaddleCenter < ball_y - 35){
-        rightPaddle_y += 6;
+    if(rightPaddleCenter < ball_y - 40){
+        rightPaddle_y += 8;
     }
-    else if(rightPaddleCenter > ball_y + 35){
-        rightPaddle_y -= 6;
+    else if(rightPaddleCenter > ball_y + 40){
+        rightPaddle_y -= 8;
     }
 }
 
 function movement(){
+    if(winScreen){
+        return;
+    }
     let delta_y;
     ball_x += ballSpeed_x;
     ball_y += ballSpeed_y;
@@ -102,25 +121,75 @@ function movement(){
     }
 }
 
+function drawNet(){   
+    for(let i = 0; i < canvas.height - 39; i += 34.5){        
+        drawRect(canvas.width / 2 -1, i + 10, 2, 28, 'white');
+    }
+}
+
 
 function draw(){
+    let center = 450;
 
-    //turf
-    c.fillStyle = 'green';
-    c.fillRect(0, 0, canvas.width, canvas.height);
+    //turf    
+    drawRect(0, 0, canvas.width, canvas.height, 'green');
+    
+    c.beginPath()
+    c.strokeStyle = 'white';
+    drawLine(10, 10, canvas.width - 10, 10);
+    drawLine(canvas.width - 10, 10, canvas.width - 10, canvas.height - 10);
+    drawLine(canvas.width - 10, canvas.height - 10, 10, canvas.height - 10);
+    drawLine(10, canvas.height - 10, 10, 10);
+    c.stroke()
+    
 
-    //paddles
-    c.fillStyle = 'white';
-    c.fillRect(2, leftPaddle_y, 10, PADDLE_HEIGHT);
-    c.fillRect(canvas.width - 12, rightPaddle_y, 10, PADDLE_HEIGHT)
+    if(winScreen){
+        c.fillStyle = 'white';
+        if(leftPlayerScore >= WINNING_SCORE){
+            c.fillText('Left Player Won!!!', center, 100);
+        }
+        else if(rightPlayerScore >= WINNING_SCORE){
+            c.fillText('Right Player Won!!!', center, 100);
+        }
+        c.fillText('click to reset game', center, 400);
+        return;
+    }
 
+
+    //paddles   
+    drawRect(2, leftPaddle_y, 10, PADDLE_HEIGHT, 'white');
+    drawRect(canvas.width - 12, rightPaddle_y, 10, PADDLE_HEIGHT, 'white')
+
+    //players score
     c.fillText(leftPlayerScore, 100, 100);
     c.fillText(rightPlayerScore, canvas.width - 100, 100);
 
-    //ball
-    c.fillStyle = 'white';
-    c.beginPath();    
-    c.arc(ball_x, ball_y, ballRadius, 0, Math.PI * 2, true);
-    c.fill();
+    drawNet()   
 
+    //center
+    drawCircle(canvas.width / 2, canvas.height / 2, 80,'green', 'white'); 
+
+    //ball    
+    drawCircle(ball_x, ball_y, ballRadius,'white', 'white');   
+   
+}
+
+function drawLine(from_x, from_y, to_x, to_y){
+    c.moveTo(from_x, from_y);
+    c.lineTo(to_x, to_y);    
+}
+
+function drawCircle(center_x, center_y, radius, fillcolor, lineColor){    
+    c.beginPath();   
+    c.strokeStyle = lineColor;
+    c.fillStyle = fillcolor; 
+    c.arc(center_x, center_y, radius, 0, Math.PI * 2, true);
+    c.fill();    
+    c.stroke();
+}
+
+function drawRect(left_x, top_y, width, height, fillColor, lineColor){
+    c.strokeStyle = lineColor;
+    c.fillStyle = fillColor;
+    c.fillRect(left_x, top_y, width, height);
 }
